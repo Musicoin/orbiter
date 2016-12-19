9d3cc234dd71c3a136fb8d35fa4ef2cac52837ce
isaac <isaac.mao@gmail.com>
2016-12-20 00:11:55 +0800

    refactory to a pure explorer

---
+++ public/js/controllers/TxController.js
@@ -0,0 +1,28 @@
+angular.module('BlocksApp').controller('TxController', function($stateParams, $rootScope, $scope, $http, $location) {
+    $scope.$on('$viewContentLoaded', function() {   
+        // initialize core components
+        App.initAjax();
+    });
+
+    $rootScope.$state.current.data["pageSubTitle"] = $stateParams.hash;
+    $scope.hash = $stateParams.hash;
+    $scope.tx = {"hash": $scope.hash};
+
+    //fetch from db
+    $http({
+      method: 'POST',
+      url: '/tx',
+      data: {"tx": $scope.hash}
+    }).success(function(data) {
+      console.log(data)
+      if (data.error)
+        $location.path("/err404/transaction/" + $scope.hash);
+      else {
+        $scope.tx = data;
+        if (data.timestamp)
+          $scope.tx.datetime = new Date(data.timestamp*1000); 
+      }
+    });
+
+
+})
---
+++ routes/filters.js
@@ -0,0 +1,91 @@
+'use strict';
+var etherUnits = require(__lib + "etherUnits.js")
+var BigNumber = require('bignumber.js');
+/*
+  Filter an array of TX
+*/
+function filterTX(txs, value) {
+  return txs.map(function(tx){
+    return [tx.transactionHash, tx.blockNumber, tx.action.from, tx.action.to,
+            etherUnits.toEther(new BigNumber(tx.action.value), 'wei'), tx.action.gas, tx.timestamp]
+  })
+}
+
+function calEth(value) {
+    return etherUnits.toEther(new BigNumber(value), 'wei');
+}
+
+function filterTX2(txs) {
+  return txs.map(function(tx) {
+    var ttx = tx;
+    ttx.value = etherUnits.toEther(new BigNumber(tx.value), 'wei');
+    return ttx;
+  });
+}
+
+
+function filterBlock(block, field, value) {
+  var tx = block.transactions.filter(function(obj) {
+    return obj[field]==value;
+  });
+  tx = tx[0];
+  if (typeof tx !== "undefined")
+    tx.timestamp = block.timestamp;
+  return tx;
+}
+
+/* extract transactions from blocks */
+function extractTX(txs) {
+
+  return txs.map(function(tx) {
+    var ttx = tx.action;
+    ttx.value = etherUnits.toEther(new BigNumber(tx.action.value), 'wei');
+    ttx.timestamp = tx.timestamp;
+    ttx.hash = tx.transactionHash;
+    return ttx;
+  });
+}
+
+/* make blocks human readable */
+function filterBlocks(blocks) {
+  return blocks.map(function(block) {
+    var b = block;
+    b.extraData = hex2ascii(block.extraData);
+    return b;
+  })
+}
+
+/* stupid datatable format */
+function datatableTX(txs) {
+  return txs.map(function(tx){
+    return [tx.hash, tx.blockNumber, tx.from, tx.to,
+            etherUnits.toEther(new BigNumber(tx.value), 'wei'), tx.gas, tx.timestamp]
+  })
+}
+
+function internalTX(txs) {
+  return txs.map(function(tx){
+    return [tx.transactionHash, tx.blockNumber, tx.action.from, tx.action.to,
+            etherUnits.toEther(new BigNumber(tx.action.value), 'wei'), tx.action.gas, tx.timestamp]
+  })
+}
+
+
+var hex2ascii = function (hexIn) {
+    var hex = hexIn.toString();
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+module.exports = {
+  extractTX: extractTX,
+  filterBlock: filterBlock,
+  filterBlocks: filterBlocks,
+  filterTX: filterTX,
+  filterTX2: filterTX2,
+  datatableTX: datatableTX,
+  internalTX: internalTX,
+  calEth: calEth
+}
