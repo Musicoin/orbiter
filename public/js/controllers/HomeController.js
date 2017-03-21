5e54b623cc5bfac1c0f4610ab9b6ba41c3ce1d62
Money <Pete.Money.ext@dematic.com>
2017-03-21 16:31:35 -0400

    Add stats widget to home.html

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -7,20 +7,52 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
     var URL = '/data';
 
     $rootScope.isHome = true;
+    $scope.current_block_number = 0;
 
     $scope.autorefresh= function() {
-      $scope.rand = 10;
+      $scope.refresh_timer = 15;
 
       (function update() {
-        $scope.rand--;
+        $scope.refresh_timer--;
         $timeout(update, 1000);
-        if ($scope.rand==0){
+        if ($scope.refresh_timer==0){
           $scope.reloadBlocks();
           $scope.reloadTransactions();
-          $scope.rand = 10
+          $scope.refresh_timer = 15
         }
       }());
     }
+
+    // Fetch the complete info on the latest block for difficulty and other stats
+    $scope.getLatestBlockInfo = function(blockNum) {
+        $http({
+          method: 'POST',
+          url: '/web3relay',
+          data: {"block": blockNum}
+        }).success(function(data) {
+          if (data.error)
+            $location.path("/err404/block/" + blockNum);
+          else {
+                $scope.getSecondToLastBlockInfo(blockNum -1);                
+                $scope.last_block = data;
+          }
+        });
+    }
+
+    // This is ugly, and violates the DRY principle, but it works for now until we can refactor it
+    $scope.getSecondToLastBlockInfo = function(blockNum) {
+        $http({
+          method: 'POST',
+          url: '/web3relay',
+          data: {"block": blockNum}
+        }).success(function(data) {
+          if (data.error)
+            $scope.second_to_last_block = $scope.last_block;
+          else
+            $scope.second_to_last_block = data;
+        });
+    }
+
 
     $scope.reloadBlocks = function() {
       $scope.blockLoading = true;
@@ -31,6 +63,14 @@ angular.module('BlocksApp').controller('HomeController', function($rootScope, $s
       }).success(function(data) {
         $scope.blockLoading = false;
         $scope.latest_blocks = data.blocks;
+        
+        if ($scope.current_block_number < data.blocks[0].number)
+        {
+            $scope.current_block_number = data.blocks[0].number;
+            $scope.getLatestBlockInfo(data.blocks[0].number);
+        }
+
+        
       });
     }
 
