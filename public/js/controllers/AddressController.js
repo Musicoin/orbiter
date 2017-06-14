17379a74a5e09d7375159aa8e784d32869230f9f
isaac <isaac.mao@gmail.com>
2017-06-14 11:03:48 +0800

    solving issue #26 with removing all option

--- public/js/controllers/AddressController.js
+++ public/js/controllers/AddressController.js
@@ -58,8 +58,8 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
           data: { "addr": $scope.addrHash, "count": count }
         },
         "lengthMenu": [
-                    [10, 20, 50, 100, 150, -1],
-                    [10, 20, 50, 100, 150, "All"] // change per page values here
+                    [10, 20, 50, 100, 150],
+                    [10, 20, 50, 100, 150] // change per page values here
                 ],
         "pageLength": 20,
         "order": [
